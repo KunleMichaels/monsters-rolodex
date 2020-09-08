6ed16394e17fe4c5a8943aebad0a8d79ebb88443
KunleMichaels <ksquare267@gmail.com>
2020-09-08 08:28:00 +0100

    Updates

---
+++ precache-manifest.bff056138d6d52f188ee521b3bbdbb87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a171d85d498ace89095c76280bd33f5e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c6818de7df996397979",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "4c6818de7df996397979",
+    "url": "/monsters-rolodex/static/js/main.c0b3bc59.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
